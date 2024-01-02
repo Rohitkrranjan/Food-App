9f9b3802e5189bff8a7bd5d1dac870f516672764
Rohitkrranjan <ranjanrohitkumar498@gmail.com>
2024-01-02 15:47:36 +0530

    header page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import './Header/Header.css'
 
-const Header = ()=>{
+const Header = () => {
+  const user = {
+    name: "Rohit",
+  };
 
-    return(
-        <div>
-            <h1>This is Header part</h1>
-        </div>
-    )
+  const cart = {
+    totalCount: 10,
+  };
+
+  const logout = () => {};
+
+  return(
+    <header className="header">
+    <div className="main_header">
+    <Link to="/" className="logo">
+          Food App!
+    </Link> 
+       <nav>
+           <ul>
+              {user ? (
+                <li className="menu_container">
+                  <Link to="/profile">{user.name}</Link>
+                  <div className="menu">
+                  <Link to="/profile"> profile</Link>
+                  <Link to="/orders">Orders</Link>
+                  <a onClick={logout}>Logout</a>
+                 </div>
+                </li>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
+
+        <li>
+            <Link to="/cart">
+                Cart
+                {cart.totalCount > 0 && <span className="cart_count">{cart.totalCount}</span>}
+            </Link>
+        </li>
+      </ul>
+    </nav>
+    </div>
+    </header>
+  )
 }
 export default Header;
